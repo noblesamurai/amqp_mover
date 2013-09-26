d5eadd1c6a25a10bd7063f464573359661a7d84c
Tim Allen <tim@noblesamurai.com>
2013-09-26 16:24:33 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+express_amqp_publisher
+======================
+Move messages from one queue to another, even across AMQP instances.
---
+++ index.js
@@ -0,0 +1,45 @@
+var amqp = require('amqplib'),
+    Q = require('q').
+
+var amqp_read_url = process.env.AMQP_READ_URL;
+var amqp_write_url = process.env.AMQP_WRITE_URL;
+
+var write_chan;
+var write_init =
+  Q(amqp.connect(amqp_write_url))
+  .then(function (conn) {
+    console.log('write connection up');
+    return conn.createChannel();
+  })
+  .then(function (ch) {
+    write_chan = ch;
+    return Q.all([
+      ch.assertQueue('foo'),
+      ch.assertExchange('bar'),
+      ch.bindQueue('foo', 'bar', 'baz')
+    ]);
+  });
+
+function handleMessage(message) {
+  write_init.then(function() {
+    console.log('writing message:');
+    console.log(message);
+    write_chan.publish('bar', 'baz', message.content);
+  });
+}
+
+var read_init =
+  Q(amqp.connect(amqp_read_url))
+  .then(function (conn) {
+    console.log('read connection up');
+    return conn.createChannel();
+  })
+  .then(function (ch) {
+    chan = ch;
+    return Q.all([
+      ch.assertQueue('foo'),
+      ch.assertExchange('bar'),
+      ch.bindQueue('foo', 'bar', 'baz'),
+      ch.consume('foo', handleMessage, {noAck: true})
+    ]);
+  });
